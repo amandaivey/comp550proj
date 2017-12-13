03d8e5a0d48aa1e38d43e8109c8103e9417637c0
amandaivey <amanda.ivey@mail.mcgill.ca>
2017-12-13 18:00:20 -0500

    tensorize tokenized tweets

--- practice.py
+++ practice.py
@@ -3,10 +3,20 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import tweepy
 import sys
+import string
 import nltk
 import csv
 from nltk.tokenize import TweetTokenizer
-
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import string
+from gensim.models import Word2Vec
+import gensim
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 consumer_token = "7QclmKZ0uYE1guPPbmqjZ6i8v"
 consumer_secret = "ijFGLclKhx2eOkXrmq0NH1z5cFSjugt97e0x9kCexCbutoFOoc"
@@ -15,7 +25,9 @@ access_secret = "00rtcn0Rwff0FyTbr5xCuvUfplVH8TALeLNktUSqI10ev"
 
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-file_to_read = sys.argv[1]
+#file_to_read = sys.argv[1]
+
+punct = string.punctuation
 
 #reads a tab delimited text file of the format tweet id, topic, rating
 #and puts this info into a python dict of the form tweetid: rating
@@ -67,8 +79,48 @@ def open_csv():
         for row in reader:
             tweet_id = row[0]
             tweet = row[1]
-            tweet_dict[tweet_id] = tweet
+            no_punc = tweet.translate(None, string.punctuation)
+            tweet_dict[tweet_id] = no_punc.lower()
     return tweet_dict
+
+#generates a corpus, a list of lists the inner list is a tokenized tweet representing a single sample
+#returns this list to be vectorized
+def generate_corpus(tweet_dict):
+    corpus = []
+    for k,v in tweet_dict.iteritems():
+        tweet = ""
+        for word in v:
+            no_punc = (word.rstrip(punct))
+            tweet = tweet + no_punc + " "
+            #no_punc = tweet.translate(string.punctuation)
+        corpus.append(tweet)
+    return corpus
+
+#generates a corpus vector that will be able to be used to generate feature vectors for each tweet sample
+def vectorize_corpus(corpus, tweet_dict):
+    vectorizer = CountVectorizer()
+    #print corpus
+    X = vectorizer.fit_transform(corpus)
+    return X
+
+#returns a list of lists where the outer list is a sample containing a list of tokens in each sample
+def setup_corpus_for_w2v(tokenized_tweets):
+    sample_list = []
+    for tweet in tokenized_tweets.itervalues():
+        sample_list.append(tweet)
+    return sample_list
+
+#takes the tokenized sentences and word2vec model and forms tensors for each sentence
+#will require padding
+def tensorize_samples(tokenized_tweets, model):
+    tensors = []
+    max_length = 0
+    for tweet in tokenized_tweets.itervalues():
+        V_data = []
+        for word in tweet:
+            V_data.append(model.wv[word])
+        tensors.append(V_data)
+    return tensors
 
 
 def main():
@@ -81,18 +133,37 @@ def main():
     #print(tweet)
 
     #dictionary of ids to rating so that you can find the rating of a given tweet
-    id_dict = read_file(file_to_read)
-    print id_dict
+    #id_dict = read_file(file_to_read)
+    #print id_dict
 
     #reads the csv file and puts the tweets into a dictionary of tweet_id: tweet
     tweet_dict = open_csv()
-    print tweet_dict
+    #print tweet_dict
 
     #tokenized_tweets is a dictionary of tweet_id : tokenized_tweet
     tokenized_tweets = tokenize_tweets(tweet_dict)
-    print tokenized_tweets
+    #print tokenized_tweets
 
     #write_to_csv(tweet_dict, '/Users/amandaivey/PycharmProjects/comp550proj/twitter_data.csv')
+    corpus = generate_corpus(tokenized_tweets)
+    #print corpus
+    vectorize_corpus(corpus, tokenized_tweets)
+
+    corpus_for_w2v = setup_corpus_for_w2v(tokenized_tweets)
+
+    model = gensim.models.Word2Vec(corpus_for_w2v, min_count=1,size=100,workers=4)
+
+    print "Similarity Between Man and Woman: "
+    print(model.similarity('man', 'woman'))
+    word = ["man", "woman"]
+    V_data = model.wv[word]
+    V = torch.Tensor(V_data)
+    
+    tensorize_samples(tokenized_tweets, model)
+    print "Three most common words in corpus"
+    print(model.wv.index2word[0], model.wv.index2word[1], model.wv.index2word[2])
+    print punct
+
 
 
 if __name__ == '__main__':
