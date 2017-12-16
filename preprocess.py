c99601d5e4395ba5d50ef69d981c954bababfdc6
Theodore Morley <theodore.morley@mail.mcgill.ca>
2017-12-16 14:55:05 -0500

    Added twitter aware tokenizer

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ A set of functions for preprocessing data before moving it into the model
 import numpy
 from sklearn import utils
 from sklearn import model_selection
+from nltk.tokenize import TweetTokenizer
 
 def shuffle(data, targets, size_test):
     shuffler = model_selection.ShuffleSplit(n_splits = 1, test_size=size_test)
@@ -22,6 +23,24 @@ def shuffle(data, targets, size_test):
             test.append(data[test_index])
             test_targ.append(targets[test_index])
     return ((train, train_targ), (test, test_targ))
+
+def tokenize(data):
+    toked = []
+    tknzr = TweetTokenizer()
+    for d in data:
+        toked.append(tknzr[d])
+    return toked
+
+'''
+Input:
+    dataset: Iterable containing all the data to be trained on
+    targets: Indexed iterable matching ratings/targets to the datapoints in dataset
+output:
+    data: Same as above, but tokenized, shuffled, etc
+    targs: Same as above
+'''
+def full_preprocess(dataset, targets):
+    return True
 
 def main():
     X = [[0,0,1],[1,1,0]]
